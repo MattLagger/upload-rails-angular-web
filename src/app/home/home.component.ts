5e03a202c4692bf7296c8a76f8712d0a97eb8747
Asterom <wendreo95@outlook.com>
2017-08-22 12:41:34 -0300

    Discernimento de post e upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,13 +34,21 @@ export class HomeComponent {
 
   	}
 
-  	save(): void{
+  	save(haveFile){
+  		if (haveFile) { 
   			const event: UploadInput = {
   			type: 'uploadAll',
   			url: 'http://localhost:3000/photos',
   			method: 'POST',
   			data: {data: ""}
   		};
+  		event.data.data = JSON.stringify(this.photo);
+  		this.uploadInput.emit(event)
+  		} else {
+  			this.photoService.addPhoto(this.photo).subscribe(data => {
+  				console.log("data");
+  			});
+  		}
   	}
 
   	removeFile(): void{
